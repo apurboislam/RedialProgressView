ad4cbfd825b766b5485636ad05f3d2ee1e1194de
apurboislam <apurbo161221@gmail.com>
2023-06-30 11:05:06 +0600

    RedialProgressView

---
+++ RadialProgressView/src/main/java/com/softbahi/radialprogressview/RadialProgressDialog.java
@@ -0,0 +1,27 @@
+package com.softbahi.radialprogressview;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.widget.LinearLayout;
+
+public class RadialProgressDialog {
+    static ProgressDialog coreprog;
+    public static void showRadialProgressDialog(Context contextx, boolean show){
+        if (show) {
+            if (coreprog == null) {
+                coreprog = new ProgressDialog(contextx);
+                coreprog.setCancelable(false);
+                coreprog.setCanceledOnTouchOutside(false);
+                coreprog.requestWindowFeature(1);
+                coreprog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+            }
+           coreprog.show();
+           coreprog.setContentView(R.layout.progress_dialog);
+            ((LinearLayout) coreprog.findViewById(R.id.linear)).addView(new RadialProgressView(contextx));
+        } else if (coreprog != null) {
+            coreprog.dismiss();
+        }
+
+    }
+}
